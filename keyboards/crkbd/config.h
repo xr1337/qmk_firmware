077b90369629feb21ec83f73843a83b216c09830
Sufiyan Yasa <x1337@gmail.com>
2022-07-20 07:04:19 +0800

    Home row mods setup
    reference: https://precondition.github.io/home-row-mods

--- keyboards/crkbd/config.h
+++ keyboards/crkbd/config.h
@@ -68,5 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #define USE_SERIAL
